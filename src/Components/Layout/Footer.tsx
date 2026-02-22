bd4bb260542e333b4472df7b96c87e7847a207de
AnisulHaqueNiloy <niloy2931@gmail.com>
2026-02-22 15:43:21 +0600

    footer has been completed

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -1,8 +1,135 @@
+import logo from "../../assets/logo.png";
+import rightimg from "../../assets/Group 1.png";
+import { FaFacebookF, FaInstagram, FaTwitter, FaTiktok } from "react-icons/fa";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="px-4 md:px-10  bg-[#EAE9E5] md:mt-32 mt-11 ">
+      <footer className="w-full  ">
+        {/* 1. Join Club Section (Blue Box) */}
+        <div className="bg-[#4A69E2] md:rounded-tr-[48px] rounded-tr-3xl rounded-tl-3xl md:rounded-tl-[48px] p-8 md:p-16 flex flex-col md:flex-row justify-between items-center overflow-hidden relative">
+          <div className="z-10 w-full md:w-1/2 md:pb-10 pb-16">
+            <h2 className="text-white text-3xl md:text-5xl font-semibold rubik-600 uppercase rubik-700 leading-tight mb-4">
+              Join our KicksPlus <br /> Club & get 15% off
+            </h2>
+            <p className="text-[#E7E7E3] text-lg mb-8 opens-600 font-semibold">
+              Sign up for free! Join the community.
+            </p>
+            <div className="flex flex-row gap-2 w-full">
+              <input
+                type="email"
+                placeholder="Email address"
+                className="bg-transparent border border-white/40 text-[#E7E7E3] p-3 rounded-xl focus:outline-none "
+              />
+              <button className="bg-[#232321] text-[10x] md:text-[14px] cursor-pointer rubik-500 text-white px-3 md:px-8 py-3 rounded-xl  uppercase hover:bg-black transition-all">
+                Submit
+              </button>
+            </div>
+          </div>
 
-export default Footer
+          {/* Big KICKS Logo with Image Background */}
+          <div className="flex w-full md:justify-center">
+            <img src={rightimg} alt="KicksPlus Logo" className="w-40 h-40 md:w-60 md:h-60 object-contain" />
+          </div>
+        </div>
+
+        {/* 2. Main Footer Links Section (Dark Box) */}
+        <div className="bg-[#232321] relative md:rounded-tr-[48px] rounded-tr-3xl rounded-tl-3xl md:rounded-tl-[48px] rounded-br-3xl rounded-bl-3xl -mt-20 md:rounded-br-[48px] md:rounded-bl-[48px]  z-50 pt-24 pb-10 px-8 md:px-16">
+          <div className="grid grid-cols-1 md:grid-cols-5 md:gap-16 gap-5 text-white">
+            {/* About Us */}
+            <div className="md:col-span-2">
+              <h4 className="text-[#FFA52F] rubik-600 font-semibold md:text-4xl text-2xl mb-4">
+                About us
+              </h4>
+              <p className="text-[#E7E7E3] opens-600 font-semibold md:text-xl text-lg leading-relaxed">
+                We are the biggest hyperstore in the universe. We got you all
+                cover with our exclusive collections and latest drops.
+              </p>
+            </div>
+
+            {/* Categories */}
+            <div>
+              <h4 className="text-[#FFA52F] md:text-2xl text-lg rubik-600 font-semibold mb-4">
+                Categories
+              </h4>
+              <ul className="space-y-2 text-[#E7E7E3] opens-600 text-lg md:text-xl font-semibold">
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Runners
+                </li>
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Sneakers
+                </li>
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Basketball
+                </li>
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Outdoor
+                </li>
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Golf
+                </li>
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Hiking
+                </li>
+              </ul>
+            </div>
+
+            {/* Company */}
+            <div>
+              <h4 className="text-[#FFA52F] md:text-2xl text-lg rubik-600 font-semibold mb-4">
+                Company
+              </h4>
+              <ul className="space-y-2 text-[#E7E7E3] opens-600 text-lg md:text-xl font-semibold">
+                <li className="hover:text-white cursor-pointer transition-all">
+                  About
+                </li>
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Contact
+                </li>
+                <li className="hover:text-white cursor-pointer transition-all">
+                  Blogs
+                </li>
+              </ul>
+            </div>
+
+            {/* Follow Us */}
+            <div>
+              <h4 className="text-[#FFA52F] md:text-2xl text-lg rubik-600 font-semibold mb-4">
+                Follow us
+              </h4>
+              <div className="flex gap-4">
+                <FaFacebookF
+                  className="cursor-pointer hover:text-[#4A69E2] transition-all"
+                  size={24}
+                />
+                <FaInstagram
+                  className="cursor-pointer hover:text-[#4A69E2] transition-all"
+                  size={24}
+                />
+                <FaTwitter
+                  className="cursor-pointer hover:text-[#4A69E2] transition-all"
+                  size={24}
+                />
+                <FaTiktok
+                  className="cursor-pointer hover:text-[#4A69E2] transition-all"
+                  size={24}
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* Bottom Big Logo */}
+          <div className="md:mt-20 mt-10 flex justify-center  pt-10">
+            <img
+              src={logo}
+              alt="KICKS"
+              className="w-full h-auto object-contain z-50"
+            />
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Shared/NewDrops.tsx
+++ src/Components/Shared/NewDrops.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import "swiper/css/pagination";
+import 'swiper/css/grid';
+import { Link, useLocation } from "react-router-dom";
 // Swiper Components এবং Styles
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination,Grid } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
 const ProductSection = () => {
   const location = useLocation();
-  // যদি পাথ '/' (home) হয় তবে একরকম, অন্যথায় 'You may also like'
+ 
   const isHomePage = location.pathname === "/";
 
   const products = [
@@ -60,18 +63,20 @@ const ProductSection = () => {
           )}
         </div>
 
-     
         {isHomePage ? (
           <button className="bg-[#4A69E2] cursor-pointer text-white md:px-8 px-3 md:py-3 py-2 w-fit rubik-500 text-sm rounded-lg hover:scale-110 transition-transform font-bold uppercase hover:bg-blue-700 transition-all active:scale-95">
             Shop Now
           </button>
         ) : (
           <div className="flex gap-2">
-            <button className="prev-btn bg-gray-400 hover:bg-black text-white p-3 rounded-lg transition-all">
-              &lt;
+            {/* Prev Button */}
+            <button className="prev-btn cursor-pointer bg-[#70706E] hover:bg-black text-white md:w-[40px] md:h-[40px] w-[32px] h-[32px] rounded-lg transition-all flex items-center justify-center">
+              <MdKeyboardArrowLeft size={24} />
             </button>
-            <button className="next-btn bg-[#232321] text-white p-3 rounded-lg transition-all">
-              &gt;
+
+            {/* Next Button */}
+            <button className="next-btn cursor-pointer bg-black text-white md:w-[40px] md:h-[40px] w-[32px] h-[32px] rounded-lg transition-all flex items-center justify-center border border-gray-200">
+              <MdKeyboardArrowRight size={24} />
             </button>
           </div>
         )}
@@ -82,7 +87,7 @@ const ProductSection = () => {
           {products.slice(0, 4).map((product) => (
             <div key={product.id} className="flex flex-col">
               {/* Card Body */}
-              <div className="bg-[#FAFAFA] rounded-[20px] md:rounded-[28px] p-2 md:p-4 border-2 md:border-4 border-white aspect-square relative overflow-hidden group">
+              <div className="bg-[#FAFAFA] rounded-[20px] md:rounded-[24px] p-2 md:p-5 border-2 md:border-4 border-white aspect-square relative overflow-hidden group">
                 <span className="absolute top-0 left-0 rubik-600 font-semibold bg-[#4A69E2] text-white px-2 md:px-4 py-1 md:py-2 rounded-br-xl md:rounded-br-3xl text-xs  ">
                   New
                 </span>
@@ -95,13 +100,15 @@ const ProductSection = () => {
 
               {/* Product Info */}
               <div className="mt-3 md:mt-4">
-               
-                <h3 className="font-bold text-sm md:text-xl uppercase text-[#232321] mb-2 md:mb-3 h-10 md:h-14 overflow-hidden line-clamp-2">
+                <h3 className="font-semibold text-sm md:text-2xl rubik-600 uppercase text-black mb-2 md:mb-3 h-10 md:h-14 overflow-hidden line-clamp-2">
                   {product.name}
                 </h3>
-                <button className="w-full bg-[#232321] text-white py-2 md:py-3 rounded-lg md:rounded-xl font-bold uppercase text-[10px] md:text-sm">
-                  View - <span className="text-[#FFA52F]">{product.price}</span>
-                </button>
+                <Link to={"/home"}>
+                  <button className="cursor-pointer w-full bg-[#232321] text-white py-2 md:py-3 rounded-lg md:rounded-xl rubik-500 uppercase text-[12px] md:text-sm">
+                    View Product -{" "}
+                    <span className="text-[#FFA52F]">{product.price}</span>
+                  </button>
+                </Link>
               </div>
             </div>
           ))}
@@ -109,35 +116,57 @@ const ProductSection = () => {
       ) : (
         //  Product Carousel (Swiper)
         <Swiper
-    modules={[Navigation]}
-    spaceBetween={15}
-    slidesPerView={2}
-    navigation={{
-      prevEl: ".prev-btn",
-      nextEl: ".next-btn",
-    }}
-    breakpoints={{
-      1024: { slidesPerView: 4 },
-    }}
-    className="mySwiper"
-  >
-    {products.map((product) => (
-      <SwiperSlide key={product.id}>
-         {/* সেম ডিজাইন এখানেও থাকবে */}
-         <div className="flex flex-col">
-            <div className="bg-[#FAFAFA] rounded-[20px] p-2 border-2 border-white aspect-square relative overflow-hidden group">
-               <img src={product.img} className="w-full h-full object-contain" />
-            </div>
-            <div className="mt-3">
-               <h3 className="font-bold text-[12px] uppercase text-[#232321] line-clamp-2 h-10">{product.name}</h3>
-               <button className="w-full bg-[#232321] text-white py-2 rounded-lg font-bold uppercase text-[10px]">
-                  View - <span className="text-[#FFA52F]">{product.price}</span>
-               </button>
-            </div>
-         </div>
-      </SwiperSlide>
-    ))}
-  </Swiper>
+          modules={[Navigation, Pagination,Grid]}
+          spaceBetween={15}
+          slidesPerView={2}
+          grid={{
+            rows: 2,
+            fill: 'row',
+          }}
+          pagination={{
+            clickable: true,
+            el: ".custom-pagination",
+          }}
+          navigation={{
+            prevEl: ".prev-btn",
+            nextEl: ".next-btn",
+          }}
+          breakpoints={{
+            1024: { slidesPerView: 4,
+              grid: { rows: 1 },
+             },
+          }}
+          className="mySwiper"
+        >
+          {products.map((product) => (
+            <SwiperSlide key={product.id}>
+              <div className="flex flex-col">
+                <div className="bg-[#FAFAFA] rounded-[20px] md:rounded-[24px] p-2 md:p-5 border-2 md:border-4 border-white aspect-square relative overflow-hidden group">
+                  <span className="absolute top-0 left-0 rubik-600 font-semibold bg-[#4A69E2] text-white px-2 md:px-4 py-1 md:py-2 rounded-br-xl md:rounded-br-3xl text-xs  ">
+                    New
+                  </span>
+                  <img
+                    src={product.img}
+                    alt={product.name}
+                    className="w-full h-full object-contain group-hover:rotate-[-15deg] transition-transform duration-500"
+                  />
+                </div>
+                <div className="mt-3">
+                  <h3 className="font-semibold text-sm md:text-2xl rubik-600 uppercase text-black mb-2 md:mb-3 h-10 md:h-14 overflow-hidden line-clamp-2">
+                    {product.name}
+                  </h3>
+                  <Link to={"/home"}>
+                    <button className="cursor-pointer w-full bg-[#232321] text-white py-2 md:py-3 rounded-lg md:rounded-xl rubik-500 uppercase text-[12px] md:text-sm">
+                      View Product -{" "}
+                      <span className="text-[#FFA52F]">{product.price}</span>
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+          <div className="custom-pagination mt-8 flex justify-center gap-2"></div>
+        </Swiper>
       )}
     </section>
   );
